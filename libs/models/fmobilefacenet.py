13b89ee9275b93fc818a9ec101444a41e84dd7a5
Wang Rui <2381892713@qq.com>
2019-11-19 11:11:53 +0800

    update

--- libs/models/fmobilefacenet.py
+++ libs/models/fmobilefacenet.py
@@ -63,9 +63,10 @@ def get_call_func(y_true, y_pred, config):
 
 
 def margin_softmax(embedding, y_true, config):
-    def mu(x):
-        return embedding*s
+
     s = config["loss_s"]
+    def mu(x):
+        return x*s
     #nembedding = keras.layers.Lambda(lambda x:  embedding*s)(embedding)
     nembedding = keras.layers.Lambda(mu)(embedding)
     return nembedding
